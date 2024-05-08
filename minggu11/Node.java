eb4c756a9497fcb3eb3201af1502b991bee1c14a
abhixyz1 <abhinayazrf@gmail.com>
2024-05-08 09:02:46 +0700

    Praktikum Linked List

---
+++ minggu11/Node.java
@@ -0,0 +1,15 @@
+package minggu11;
+
+/**
+ * @author Abhinaya
+ */
+
+public class Node {
+    int data;
+    Node next;
+
+    Node(int nilai, Node berikutnya) {
+        data = nilai;
+        next = berikutnya;
+    }
+}
